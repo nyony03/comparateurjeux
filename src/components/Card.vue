bfe67508030e6fdfdc5f3f4ad60e97d76492f7f5
BURILLE <cecile.burille@etu.umontpellier.fr>
2023-02-10 15:48:05 +0100

    mise en ligne de mon projet

---
+++ src/components/Card.vue
@@ -0,0 +1,49 @@
+
+
+<template>
+  <div class="Card">
+    <div class="gauche">
+      <h1>{{ titre }}</h1>
+      <p class="tag">#{{ tag }}</p>
+      <p>{{ description }}</p>
+    </div>
+    <div class="droite">
+      <img class="image" :src="img" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    titre: String,
+    tag: String,
+    description: String,
+    img: String,
+  },
+};
+</script>
+
+<style scoped>
+.Card {
+  column-count: 2;
+  color: white;
+  background-color: #363d5d;
+  padding: 10px;
+  margin: 15px 20px;
+}
+.Card:hover {
+  background-color: #5d658e;
+}
+.image:hover {
+  background-image: url("https://cdn.akamai.steamstatic.com/steam/apps/292030/header.jpg?t=1675178392");
+  z-index: 1;
+}
+h1 {
+  text-decoration: underline;
+}
+.tag {
+  font-style: italic;
+}
+</style>
---
+++ src/components/Jeux.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Jeux">
+    <h1>Jeux du moments</h1>
+
+    <Card
+        v-for="item in list"
+        :key="item.titre"
+        :titre="item.titre"
+        :tag="item.tag"
+        :description="item.description"
+        :img="item.img"
+    >
+    </Card>
+  </div>
+</template>
+
+<script>
+import Card from "./Card";
+
+export default {
+  name: "Jeux",
+  props: {
+    list: Array,
+  },
+  components: { Card },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: white;
+  margin-bottom: 30px;
+}
+</style>
